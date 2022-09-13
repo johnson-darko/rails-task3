5585fdabb88ade3f7b27e9a40a755e89a4b96e50
johnson-darko <johnsondarko365@gmail.com>
2022-09-13 22:47:08 +0200

    chnages to default value

--- db/migrate/20220913203546_create_lists.rb
+++ db/migrate/20220913203546_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[7.0]
   def change
     create_table :lists do |t|
       t.string :name
-      t.integer :position
+      t.integer :position, :null => false,:default =>1
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_203546) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
-    t.integer "position"
+    t.integer "position", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
