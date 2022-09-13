b4fe0e9abf2cf17175814b1e4adc84492a684d5a
johnson-darko <johnsondarko365@gmail.com>
2022-09-13 23:42:55 +0200

    added count

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,46 @@
 class ListsController < ApplicationController
+  before_action :set_params, only: %i[show  destroy]
+
   def index
     @lists = List.all
+  end
+
+
+  def show
+    
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    
+    if @list.save!
+      redirect_to list_path(@list), notice: "Your list has been added!!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+  def destroy
+   
+    @list.destroy
+    redirect_to lists_path, notice: "Item has been deleted from the lists!!"
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_params
+    @list = List.find(params[:id])
   end
 end
--- db/migrate/20220913203546_create_lists.rb
+++ db/migrate/20220913203546_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[7.0]
   def change
     create_table :lists do |t|
       t.string :name
-      t.integer :position, :null => false,:default =>1
+      t.integer :position
       t.references :user, null: false, foreign_key: true
 
 
