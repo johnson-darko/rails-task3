e000438c9727ff42e3e0660297f4bacb1a298417
johnson-darko <johnsondarko365@gmail.com>
2022-09-14 07:59:49 +0200

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -46,7 +46,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :position)
   end
 
   def set_params
